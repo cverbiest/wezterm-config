bf8e30319c26e3b15cd150bb062c1e390e2a8ef3
Carl Verbiest <carl@nhq-melle.be>
2026-02-25 08:11:57 +0100

    use default ssh config, use os user

--- config/domains.lua
+++ config/domains.lua
@@ -1,8 +1,16 @@
+local wezterm = require('wezterm')
 local platform = require('utils.platform')
+
+local ssh_domains = {}
+for _, dom in ipairs(wezterm.default_ssh_domains()) do
+   if not dom.name:match('^SSHMUX:') then
+      table.insert(ssh_domains, dom)
+   end
+end
 
 local options = {
    -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
-   ssh_domains = {},
+   ssh_domains = ssh_domains,
 
    -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
    unix_domains = {},
@@ -12,32 +20,22 @@ local options = {
 }
 
 if platform.is_win then
-   options.ssh_domains = {
-      {
-         name = 'ssh:wsl',
-         remote_address = 'localhost',
-         multiplexing = 'None',
-         default_prog = { 'fish', '-l' },
-         assume_shell = 'Posix',
-      },
-   }
-
+   local os_user = os.getenv('USER') or os.getenv('USERNAME')
    options.wsl_domains = {
       {
          name = 'wsl:ubuntu-fish',
          distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
+         username = os_user,
+         default_cwd = '/home/' .. os_user,
          default_prog = { 'fish', '-l' },
       },
       {
          name = 'wsl:ubuntu-bash',
          distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
+         username = os_user,
+         default_cwd = '/home/' .. os_user,
          default_prog = { 'bash', '-l' },
       },
    }
 end
-
 return options
